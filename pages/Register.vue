d0748a13fbfe57c99b1447c1c12eaf7c50278c5d
edository <mohaminullah91@gmail.com>
2023-03-24 16:41:29 +0700

    mulai di 80. Login from frontend use Api

---
+++ pages/Register.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-row>
+    <v-col cols="10" offset="1" md="4" offset-md="4">
+      <v-card class="mb-2">
+        <v-toolbar color="primary" dark> Register </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              name="fullname"
+              label="Full Name"
+              type="text"
+              v-model="form.fullname"
+              :rules="rules.fullname"
+            />
+            <v-text-field
+              name="email"
+              label="Email"
+              type="email"
+              v-model="form.email"
+              :rules="rules.email"
+              @keyup="checkEmail()"
+            />
+            <v-text-field
+              name="password"
+              label="Password"
+              type="password"
+              v-model="form.password"
+              :rules="rules.password"
+            />
+            <v-text-field
+              name="retype_password"
+              label="Re-Password"
+              type="password"
+              v-model="form.retype_password"
+              :rules="rules.retype_password"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="onSubmit()" color="primary">Register</v-btn>
+        </v-card-actions>
+      </v-card>
+      <p>Kamu belum punya akun? <v-btn to="/login" plain>Login</v-btn></p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      emailExist: false,
+      form: {
+        fullname: '',
+        email: '',
+        password: '',
+        retype_password: '',
+      },
+      rules: {
+        fullname: [(v) => !!v || 'Fullname is required'],
+        email: [
+          (v) => !!v || 'Email is required',
+          (v) => /.+@.+/.test(v) || 'Email invalid',
+          (v) => !!this.emailExist || 'Email already exist',
+        ],
+        password: [
+          (v) => !!v || 'Password is required',
+          (v) => v.length >= 6 || 'Password must be at least 6 characters',
+        ],
+        retype_password: [
+          (v) =>
+            v === this.form.password ||
+            'Re-password must be same with Password',
+        ],
+      },
+    }
+  },
+  methods: {
+    checkEmail() {
+      this.$axios
+        .$post('http://localhost:3000/auth/check-email', this.form)
+        .then((response) => {
+          console.log(response)
+          this.emailExist = false
+        })
+        .catch((error) => {
+          this.emailExist = true
+          console.log(error)
+        })
+    },
+    onSubmit() {
+      this.$axios
+        .$post('http://localhost:3000/auth/register', this.form)
+        .then((response) => {
+          console.log(response)
+        })
+    },
+  },
+}
+</script>
